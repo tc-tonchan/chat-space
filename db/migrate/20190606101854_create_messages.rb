637c2b30e609df73f5d0c93882715949019383f3
HIROKUNI <tc4tonchan@gmail.com>
2019-06-06 20:20:39 +0900

    Insertion column to messages table

--- db/migrate/20190606101854_create_messages.rb
+++ db/migrate/20190606101854_create_messages.rb
@@ -1,8 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
-      t.timestamps
+      t.text           :body, null: false
+      t.string         :image
+      t.integer        :group_id, unique: true
+      t.integer        :user_id
+      t.timestamps     null: false
     end
   end
 end
