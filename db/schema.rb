0593ba62dd795bd1748309bc487a2bb0cf31ccb2
HIROKUNI <tc4tonchan@gmail.com>
2019-06-07 15:04:02 +0900

    added new migration file
    typed column and constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190606101854) do
+ActiveRecord::Schema.define(version: 20190607052536) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "body",       limit: 65535, null: false
     t.string   "image"
-    t.integer  "group_id"
-    t.integer  "user_id"
+    t.integer  "group_id",                 null: false
+    t.integer  "user_id",                  null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
