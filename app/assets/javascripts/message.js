755c4f3bd99f482d61032a96cc08954d15425e25
HIROKUNI <tc4tonchan@gmail.com>
2019-07-01 22:52:34 +0900

    Update message.js
    Asynchronous communication

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
 $(function(){
+  function buildHTML(message){
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img class= "Lower-message__image" src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="Upper-message">
+                    <div class="Upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="Upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="Lower-message">
+                    <p class="Lower-message__content">
+                      ${content}
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+  return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +32,18 @@ $(function(){
       processData: false,
       contentType: false
     })
+      .done(function(message){
+        var html = buildHTML(message);
+        $('.Messages').append(html);
+        $('.form__message').val('');
+        $('.Messages').animate({ scrollTop: $('.Messages')[0].scrollHeight});
+        return false
+      })
+      .fail(function(){
+        alert('error');
+      })
+      .always(function(data){
+        $('.form__submit').prop('disabled', false);
+      })
   })
 });
