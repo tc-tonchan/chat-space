d1cd0c8af28d38f447fde5fc083c75f18d79d8ec
HIROKUNI <tc4tonchan@gmail.com>
2019-08-03 02:28:43 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.new
+    # @messages = Message.new
     @group = Group.find(params[:group_id])
     @messages = @group.messages.includes(:user).where('id > ?', params[:id])
   end
